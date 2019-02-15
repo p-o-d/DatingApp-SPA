b471d8e2b6fe709560dd43e894b88bf09bcc5c10
Andrii Ivanenko <andriii@camtek.com>
2019-02-15 17:52:38 +0200

    + error handling

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
     this.auth.login(this.model).subscribe(next => {
       console.log('logged!');
     }, error => {
-      console.log('login error!');
+      console.log(error);
     });
   }
 
